18cde1cdab50936a5c64d7e098ce15df26484388
alex-iank <artlenk.ru@gmail.com>
2019-01-26 20:37:22 +0300

    Fixed 04 learn

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {render} from 'react-dom';
+import App from './components/App'
+
+
+
+
+
+render(<App />, document.getElementById('root'));
+
+/*
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
+*/
